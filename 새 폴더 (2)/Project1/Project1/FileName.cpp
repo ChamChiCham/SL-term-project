e3f4cb53c9f3deb017b1cb68b0949a215b7ed079
ChamChiCham@Desktop <kyjp0623@gmail.com>
2024-06-10 13:03:32 +0900

    서버 임시

---
+++ 새 폴더 (2)/Project1/Project1/FileName.cpp
@@ -0,0 +1,325 @@
+//DESKTOP-O6QFSN0\SQLEXPRESS
+
+// SQLConnect_ref.cpp  
+// compile with: odbc32.lib  
+#include <iostream>
+#include <vector>
+#include <locale>
+#include <unordered_map>
+#include <string>
+#include <codecvt>
+#include <atlconv.h>
+
+
+
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+
+#pragma comment(lib, "WS2_32.lib")
+#pragma comment(lib, "MSWSock.lib")
+
+#include <windows.h>  
+#include <sqlext.h>
+
+SQLHENV henv;
+SQLHDBC hdbc;
+SQLHSTMT hstmt;
+
+
+bool connect_database()
+{
+    SQLRETURN retcode;
+
+
+    // Allocate environment handle  
+    retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
+    setlocale(LC_ALL, "korean");
+
+    // Set the ODBC version environment attribute  
+    if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+        retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (void*)SQL_OV_ODBC3, 0);
+
+        // Allocate connection handle  
+        if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+            retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);
+
+            // Set login timeout to 5 seconds  
+            if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+                SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
+
+                // Connect to data source  
+                retcode = SQLConnect(hdbc, (SQLWCHAR*)L"SL_DB", SQL_NTS, (SQLWCHAR*)NULL, 0, NULL, 0);
+
+                // Allocate statement handle  
+                if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+                   
+					std::cout << "Database Connected" << std::endl;
+					return true;
+                }
+				SQLDisconnect(hdbc);
+            }
+			SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+        }
+        SQLFreeHandle(SQL_HANDLE_ENV, henv);
+    }
+    return false;
+}
+
+void disconnect_database()
+{
+    SQLDisconnect(hdbc);
+    SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+    SQLFreeHandle(SQL_HANDLE_ENV, henv);
+}
+
+//=================
+constexpr int PORT_NUM = 4000;
+constexpr int BUF_SIZE = 512;
+constexpr int NAME_SIZE = 36;
+
+constexpr char CS_WRITE = 0;
+constexpr char CS_READ = 1;
+constexpr char SC_GIVE = 2;
+
+#pragma pack (push, 1)
+struct CS_WRITE_PACKET {
+	unsigned char size;
+	char	type;
+	char	mess[NAME_SIZE];
+};
+
+struct CS_READ_PACKET {
+	unsigned char size;
+	char	type;
+};
+
+struct SC_GIVE_PACKET {
+	unsigned char size;
+	char	type;
+	char	mess[NAME_SIZE][10];
+};
+#pragma pack (pop)
+
+enum COMP_TYPE { OP_ACCEPT, OP_RECV, OP_SEND };
+
+class OVER_EXP {
+public:
+    WSAOVERLAPPED _over;
+    WSABUF _wsabuf;
+    char _send_buf[BUF_SIZE];
+    COMP_TYPE _comp_type;
+    OVER_EXP()
+    {
+        _wsabuf.len = BUF_SIZE;
+        _wsabuf.buf = _send_buf;
+        _comp_type = OP_RECV;
+        ZeroMemory(&_over, sizeof(_over));
+    }
+    OVER_EXP(char* packet)
+    {
+        _wsabuf.len = packet[0];
+        _wsabuf.buf = _send_buf;
+        ZeroMemory(&_over, sizeof(_over));
+        _comp_type = OP_SEND;
+        memcpy(_send_buf, packet, packet[0]);
+    }
+};
+class SESSION {
+	OVER_EXP _recv_over;
+
+public:
+	// 객체 정보
+	size_t	    _id;
+	SOCKET		_socket;
+
+	// 패킷 재조립을 위한 남은 데이터 수 저장
+	int			_prev_remain;
+
+public:
+	SESSION()
+	{
+		_id = -1;
+		_socket = 0;
+		_prev_remain = 0;
+	}
+
+	~SESSION() {}
+
+	void do_recv()
+	{
+		DWORD recv_flag = 0;
+		memset(&_recv_over._over, 0, sizeof(_recv_over._over));
+		_recv_over._wsabuf.len = BUF_SIZE - _prev_remain;
+		_recv_over._wsabuf.buf = _recv_over._send_buf + _prev_remain;
+		WSARecv(_socket, &_recv_over._wsabuf, 1, 0, &recv_flag,
+			&_recv_over._over, 0);
+	}
+
+	void do_send(void* packet)
+	{
+		OVER_EXP* sdata = new OVER_EXP{ reinterpret_cast<char*>(packet) };
+		WSASend(_socket, &sdata->_wsabuf, 1, 0, 0, &sdata->_over, 0);
+	}
+};
+
+std::unordered_map<size_t, SESSION> clients;
+
+HANDLE h_iocp;
+SOCKET g_s_socket;
+SOCKET g_c_socket;
+OVER_EXP g_a_over;
+
+size_t g_id = 0;
+
+//=================
+
+
+void disconnect(size_t c_id)
+{
+	closesocket(clients[c_id]._socket);
+	clients.erase(c_id);
+}
+
+void process_packet(size_t c_id, char* packet)
+{
+	switch (packet[1]) {
+	case CS_WRITE: {
+		std::cout << "cs_write" << std::endl;
+		CS_WRITE_PACKET* p = reinterpret_cast<CS_WRITE_PACKET*>(packet);
+		std::cout << p->mess << std::endl;
+		std::wstring exec{
+			L"INSERT IGNORE INTO user_table (user_name) VALUES "
+		};
+		std::string str{p->mess};
+		std::wstring wstr;
+		USES_CONVERSION;
+		wstr = std::wstring(A2W(str.c_str()));
+		exec += L"(\"" + wstr + L"\")";
+		std::wcout << exec << std::endl;
+
+
+		int retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+		if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+			// SQL 문장 실행
+			retcode = SQLExecDirect(hstmt, (SQLWCHAR*)exec.c_str(), SQL_NTS);
+			if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+				std::cout << "Data inserted successfully." << std::endl;
+			}
+			else {
+				std::cout << "Failed to insert data." << std::endl;
+			}
+			// statement 핸들 해제
+			SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+		}
+		else {
+			std::cout << "Failed to allocate a statement handle." << std::endl;
+		}
+		break;
+	}
+	case CS_READ: {
+		std::cout << "cs_read" << std::endl;
+		break;
+	}
+	}
+}
+
+int main()
+{
+    if (not connect_database()) {
+        exit(-1);
+    }
+
+    WSADATA WSAData;
+    WSAStartup(MAKEWORD(2, 2), &WSAData);
+    g_s_socket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+    SOCKADDR_IN server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT_NUM);
+    server_addr.sin_addr.S_un.S_addr = INADDR_ANY;
+    bind(g_s_socket, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr));
+    listen(g_s_socket, SOMAXCONN);
+    SOCKADDR_IN cl_addr;
+    int addr_size = sizeof(cl_addr);
+
+
+	// iocp 객체 생성
+	h_iocp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, 0);
+
+	// 서버 소켓에 연결
+	CreateIoCompletionPort(reinterpret_cast<HANDLE>(g_s_socket), h_iocp, 9999, 0);
+
+	// Accept를 위한 클라이언트 소켓 하나 생성. 
+	g_c_socket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	g_a_over._comp_type = OP_ACCEPT;
+
+	// 비동기 Accept
+	AcceptEx(g_s_socket, g_c_socket, g_a_over._send_buf, 0, addr_size + 16, addr_size + 16, 0, &g_a_over._over);
+
+
+	while (true) {
+		DWORD num_bytes;
+		ULONG_PTR key;
+		WSAOVERLAPPED* over = nullptr;
+		BOOL ret = GetQueuedCompletionStatus(h_iocp, &num_bytes, &key, &over, INFINITE);
+		OVER_EXP* ex_over = reinterpret_cast<OVER_EXP*>(over);
+		if (FALSE == ret) {
+			if (ex_over->_comp_type == OP_ACCEPT) std::cout << "Accept Error";
+			else {
+				std::cout << "GQCS Error on client[" << key << "]\n";
+				disconnect(key);
+				if (ex_over->_comp_type == OP_SEND) delete ex_over;
+				continue;
+			}
+		}
+
+		if ((0 == num_bytes) && ((ex_over->_comp_type == OP_RECV) || (ex_over->_comp_type == OP_SEND))) {
+			disconnect(key);
+			if (ex_over->_comp_type == OP_SEND) delete ex_over;
+			continue;
+		}
+
+		switch (ex_over->_comp_type) {
+		case OP_ACCEPT: {
+			size_t client_id = g_id++;
+			CreateIoCompletionPort(reinterpret_cast<HANDLE>(g_c_socket), h_iocp, client_id, 0);
+
+			clients.insert(std::make_pair(client_id, SESSION{}));
+			clients[client_id]._socket = g_c_socket;
+			clients[client_id]._id = client_id;
+			clients[client_id].do_recv();
+
+			g_c_socket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+			ZeroMemory(&g_a_over._over, sizeof(g_a_over._over));
+			AcceptEx(g_s_socket, g_c_socket, g_a_over._send_buf, 0, addr_size + 16, addr_size + 16, 0, &g_a_over._over);
+			break;
+		}
+		case OP_RECV: {
+			int remain_data = num_bytes + clients[key]._prev_remain;
+			char* p = ex_over->_send_buf;
+			while (remain_data > 0) {
+				int packet_size = p[0];
+				if (packet_size <= remain_data) {
+					process_packet(key, p);
+					p = p + packet_size;
+					remain_data = remain_data - packet_size;
+				}
+				else break;
+			}
+			clients[key]._prev_remain = remain_data;
+			if (remain_data > 0) {
+				memcpy(ex_over->_send_buf, p, remain_data);
+			}
+			clients[key].do_recv();
+			break;
+		}
+		case OP_SEND:
+			delete ex_over;
+			break;
+		}
+	}
+	closesocket(g_s_socket);
+	WSACleanup();
+
+    disconnect_database();
+}
---
+++ 새 폴더 (2)/client/client/FileName.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+
+#pragma comment(lib, "WS2_32.lib")
+#pragma comment(lib, "MSWSock.lib")
+
+#include <windows.h>  
+#include <sqlext.h>
+
+constexpr int PORT_NUM = 4000;
+constexpr int BUF_SIZE = 512;
+constexpr int NAME_SIZE = 36;
+
+constexpr char CS_WRITE = 0;
+constexpr char CS_READ = 1;
+constexpr char SC_GIVE = 2;
+constexpr const char* SERVER_ADDR = "127.0.0.1";
+
+enum COMP_TYPE { OP_ACCEPT, OP_RECV, OP_SEND };
+
+class OVER_EXP {
+public:
+	WSAOVERLAPPED _over;
+	WSABUF _wsabuf;
+	char _send_buf[BUF_SIZE];
+	COMP_TYPE _comp_type;
+	OVER_EXP()
+	{
+		_wsabuf.len = BUF_SIZE;
+		_wsabuf.buf = _send_buf;
+		_comp_type = OP_RECV;
+		ZeroMemory(&_over, sizeof(_over));
+	}
+	OVER_EXP(char* packet)
+	{
+		_wsabuf.len = packet[0];
+		_wsabuf.buf = _send_buf;
+		ZeroMemory(&_over, sizeof(_over));
+		_comp_type = OP_SEND;
+		memcpy(_send_buf, packet, packet[0]);
+	}
+};
+
+class SESSION {
+	OVER_EXP _recv_over;
+
+public:
+	size_t	    _id;
+	SOCKET		_socket;
+
+	int			_prev_remain;
+
+public:
+	SESSION()
+	{
+		_id = -1;
+		_socket = 0;
+		_prev_remain = 0;
+	}
+
+	~SESSION() {}
+
+	void do_recv()
+	{
+		DWORD recv_flag = 0;
+		memset(&_recv_over._over, 0, sizeof(_recv_over._over));
+		_recv_over._wsabuf.len = BUF_SIZE - _prev_remain;
+		_recv_over._wsabuf.buf = _recv_over._send_buf + _prev_remain;
+		WSARecv(_socket, &_recv_over._wsabuf, 1, 0, &recv_flag,
+			&_recv_over._over, 0);
+	}
+
+	void do_send(void* packet)
+	{
+		OVER_EXP* sdata = new OVER_EXP{ reinterpret_cast<char*>(packet) };
+		WSASend(_socket, &sdata->_wsabuf, 1, 0, 0, &sdata->_over, 0);
+	}
+};
+
+#pragma pack (push, 1)
+struct CS_WRITE_PACKET {
+	unsigned char size;
+	char	type;
+	char	mess[NAME_SIZE];
+};
+
+struct CS_READ_PACKET {
+	unsigned char size;
+	char	type;
+};
+
+struct SC_GIVE_PACKET {
+	unsigned char size;
+	char	type;
+	char	mess[NAME_SIZE][10];
+};
+#pragma pack (pop)
+
+SESSION session;
+
+
+int main()
+{
+	std::wcout.imbue(std::locale("korean"));
+	WSADATA WSAData;
+	WSAStartup(MAKEWORD(2, 0), &WSAData);
+	SOCKET s_socket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	SOCKADDR_IN server_addr;
+	ZeroMemory(&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT_NUM);
+	inet_pton(AF_INET, SERVER_ADDR, &server_addr.sin_addr);
+	connect(s_socket, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr));
+	session._socket = s_socket;
+	int a;
+	std::cin >> a;
+
+	if (a == 0) {
+		CS_WRITE_PACKET p;
+		p.type = CS_WRITE;
+		p.size = sizeof(p);
+		strcpy_s(p.mess, NAME_SIZE, "æ»≥Á«œººø‰");
+		session.do_send(&p);
+		session.do_recv();
+	}
+	else {
+		CS_READ_PACKET p;
+		p.type = CS_READ;
+		p.size = sizeof(p);
+		session.do_send(&p);
+		session.do_recv();
+	}
+	
+
+	closesocket(s_socket);
+	WSACleanup();
+}
